f49922a56e0bae8c161a8c18f9aba838e30a543f
LUO <2472870226@qq.com>
2025-04-27 16:48:44 +0800

    Initial submission

---
+++ HWUGSign_for_Linux_X86/sign1070m-x86-241121/1070mtestweb_linux/js/pageturning.js
@@ -0,0 +1,30 @@
+﻿
+var obj;
+var pgindex = 1; //当前页
+var allpages = 1;//总页数
+
+	function pageUp()
+	{
+	
+		if(pgindex >0)
+		{			
+			//alert(pgindex);
+			pgindex -=1;
+			obj.scrollTop = (pgindex-1) * parseInt(obj.offsetHeight); //根据高度，输出指定的页
+		}
+	}
+	
+	function pageDown()
+	{
+		if(pgindex <= allpages)
+		{									
+			pgindex +=1;
+			obj.scrollTop = (pgindex-1) * parseInt(obj.offsetHeight);			
+		}
+	}
+	
+	function pageInit()
+	{
+		obj = document.getElementById("frameContent"); //获取内容层
+		allpages = Math.ceil(parseInt(obj.scrollHeight) / parseInt(obj.offsetHeight)); //获取页面数量
+	}
